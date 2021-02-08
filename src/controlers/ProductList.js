bd9013392f70d79f00fc8c2a5e97eb18df96a54f
basiscode <dirk@basiscode.de>
2021-02-08 11:57:05 +0100

    Delition of productlist items

--- src/controlers/ProductList.js
+++ src/controlers/ProductList.js
@@ -1,24 +1,50 @@
 "use strict"
 
 const { info } = require("../api/product")
+const { on } = require("../utils/dom")
 const addProductTemplate = require("../templates/productlist/productitem.ejs")
 
 /**
  * 
- * @param {HTMLElement} productList 
+ * @param {HTMLElement} producHtmlList 
  */
-function ProductList(productList) {
-    this.producList = productList
+function ProductList(producHtmlList) {
+  this.products = []
+  this.producHtmlList = producHtmlList
 }
 
-ProductList.prototype.addProduct = function(fbcId) {
-  info(fbcId)
+ProductList.prototype.init = function() {
+  on('.bc-product-list-remove-button','click', (event) => {
+    const productId = parseInt(event.handleObj.getAttribute("data-bc-fdcid"))
+    this.removeProduct(productId)
+  })
+}
+ProductList.prototype.removeProduct = function(productId) {
+    
+  //remove from products
+  for(const index in this.products) {
+    const product = this.products[index]
+    if (product['fdcId'] == productId) {
+      this.products.splice(index, 1);
+      break
+    }
+  }
+  
+  // remove from productHtmlList
+  const htmlListItem = document.querySelector("#bc-product-list tr[data-bc-fdcid='"+productId+"']")
+  htmlListItem.remove()
+}
+
+ProductList.prototype.addProduct = function(fdcId) {
+  info(fdcId)
     .then((productInfo) => {
-      const productHtml = addProductTemplate({productname: productInfo["description"]})
-      this.producList.insertAdjacentHTML("beforeend", productHtml)
+      const productHtml = addProductTemplate({
+        productname: productInfo["description"],
+        productid: productInfo["fdcId"]
+      })
+      this.producHtmlList.insertAdjacentHTML("beforeend", productHtml)
+      this.products.push(productInfo)
     })
 }
-
-ProductList.prototype.init = function() {}
 
 module.exports = ProductList
